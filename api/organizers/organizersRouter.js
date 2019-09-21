ae234408d77cdf56216caa425ccde6134025b4cd
Tommy Coleman <tommiilee@gmail.com>
2019-09-21 17:03:16 -0400

    completed organizer endpoints and helper functions

---
+++ api/organizers/organizersRouter.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+const Organizers = require('./organizersHelper');
+
+router.get('/', (req, res) => {
+    Organizers.getOrganizers()
+    .then(result => {
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        res.status(500).json({error: err})
+    })
+})
+
+router.post('/', (req, res) => {
+    const organizer = req.body;
+    Organizers.addOrganizer(organizer)
+    .then(result => {
+        res.status(201).json(result)
+    })
+    .catch(err => {
+        res.status(500).json({error: err})
+    })
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    Organizers.getOrganizerById(id)
+    .then(organizer => {
+        res.status(200).json(organizer)
+    })
+    .catch(err => {
+        res.status(500).json({error: err})
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    Organizers.deleteOrganizer(id)
+    .then(result => {
+        res.status(200).json({message: `Successfully deleted organizer with id ${id}`})
+    })
+    .catch(err => {
+        res.status(500).json({error: err})
+    })
+})
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const organizer = req.body;
+    Organizers.updateOrganizer(id, organizer)
+    .then(result => {
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        res.status(500).json({error: err})
+    })
+})
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const server = express();
+const OrganizerRoute = require('./organizers/organizersRouter')
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
-
+server.use('/organizers', OrganizerRoute);
 module.exports = server;
